cb6e2943324fb3611c89185ef480be5036b3a2b5
nsavi <112178048+nsavi@users.noreply.github.com>
2023-05-03 09:19:50 +0530

    Update EXP_03.R

--- EXP_03.R
+++ EXP_03.R
@@ -21,3 +21,42 @@ data.frame(x,px)
 
 #(iv) Plotting
 plot(x, px, type="o",xlab="values of x",ylab="Probability distribution of x",main = "binomial distribution")
+
+#poisson distribution
+#problem 1 
+lambda = 12 #12 cars per minute 
+#X denotes the no of cars crossing the bridge per minute
+#P(X > = 17) = 1-P(C<=16)
+Prob = 1-ppois(16,lambda)
+round(Prob,4)
+
+#problem 2
+#(i)
+lambda = 1.6 #10 minutes of interval
+
+# x xdenotes the no. of customers using the ATM 
+# for any 10 minutes of interval  
+#(ii) P(X=3)
+prob_exactly_three = dpois(3,lambda)
+prob_exactly_three
+
+#(iii) P(X<=3)
+prob_three_or_fewer = ppois(3, lambda)
+prob_three_or_fewer
+
+#(iv) P(X>=3) = 1-P(X<=2)
+#Here, Lambda = 2*1.6 = 3.2
+#since it is for any 20 minutes of interval 
+prob_atleast_three = 1 - ppois(2,3.2)
+prob_atleast_three
+
+#(v)
+X = seq(0,11)
+prob = r ound(dpois(X,1.6),4)
+#Creating table
+data.frame(X,prob)
+#plotting
+plot(X,prob,type = "o")
+
+
+
